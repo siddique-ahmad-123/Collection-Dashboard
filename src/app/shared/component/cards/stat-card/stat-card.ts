38b0d438d3e350002895ed0e664d351d7e5f85ab
siddique-ahmad-123 <ahmadsiddique7073@gmail.com>
2025-12-21 14:01:51 +0530

    added services

---
+++ src/app/shared/component/cards/stat-card/stat-card.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-loan-summary-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './stat-card.html',
+  styleUrls: ['./stat-card.css']
+})
+export class StatCardComponent {
+
+   @Input() title!: string;
+
+  @Input() casesDone!: number;
+  @Input() totalCases!: number;
+
+  @Input() overdueAmount!: number;
+  @Input() totalAmount!: number;
+}
